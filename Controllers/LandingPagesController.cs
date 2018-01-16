8be21bbc3a4c76fda090ecef1e8c8032e5d0ec40
BrianJerez <35076446+BrianJerez@users.noreply.github.com>
2018-01-16 17:45:36 -0500

    Add files via upload
    school update

--- Controllers/LandingPagesController.cs
+++ Controllers/LandingPagesController.cs
@@ -2,14 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Opera.Controllers
 {
+    [Route("")]
     public class LandingPagesController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Register()
+        [Route("Registro")]
+        public IActionResult Registro()
         {
             return View();
         }
